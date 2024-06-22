b47c89f4cd15a0f3bd124dd3b569db2b44b489a1
Maciej Kucab <72211630+QuelQuel7@users.noreply.github.com>
2024-06-22 16:53:23 +0200

    debugged listener

--- netcat.py
+++ netcat.py
@@ -1,6 +1,3 @@
-#Maciej Kucab, prosty listener w pythonie 
-
-
 import sys
 import socket
 import getopt
@@ -23,10 +20,8 @@ def usage():
     print("-e --execute=<file> executes given file when connection is established")
     print("-c --command initializes a command shell")
     print("-u --upload=<dest> uploads file and writes to <dest> when connection is established")
-    print("Example : python listener.py -t 192.168.0.1 -p 4444 -l -c")
+    print("Example : python netcat.py -t 192.168.0.1 -p 4444 -l -c")
     print("")
-
-
     sys.exit(0)
 
 def main():
@@ -39,13 +34,14 @@ def main():
 
     if not len(sys.argv[1:]):
         usage()
-#UWAGA -- proba przeparsowania inputu, przyda sie do innych programow 
-    try: 
+
+    try:
         opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:cu:", ["help","listen","execute","target","port", "command", "upload"])
     except getopt.GetoptError as err:
         print(str(err))
         usage()
-    for o,a in opts: 
+
+    for o, a in opts:
         if o in ("-h", "--help"):
             usage()
         elif o in ("-l", "--listen"):
@@ -57,93 +53,101 @@ def main():
         elif o in ("-u", "--upload"):
             upload_destination = a
         elif o in ("-p", "--port"):
-            port = int(a) 
+            port = int(a)
         elif o in ("-t", "--target"):
             target = a
         else:
-            raise ValueError(f"There is something very wrong")
+            raise ValueError("There is something very wrong")
 
-#Tylko wysylanie danych
-    if not listen and len(target) and port > 0: 
+    if not listen and len(target) and port > 0:
         buffer = sys.stdin.read()
-        client_sender(buffer) #to trzeba zdefiniowac
-#Nasluch
-    if listen: 
-        server_loop() # To trzeba zdefiniowac
+        client_sender(buffer)
 
-def client_sender(buffer): 
+    if listen:
+        server_loop()
+
+def client_sender(buffer):
+    global target
+    global port
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    try: 
-        client.connect((target,port))
+    try:
+        client.connect((target, port))
+
         if len(buffer):
-            client.send(buffer)
-            print("connection established, waiting for the data")
-        while True: 
-            recv_len =1 
+            client.send(buffer.encode('utf-8'))
+            print("Connection established, waiting for the data")
+
+        while True:
+            recv_len = 1
             response = ""
-            while recv_len: 
-                data = client_recv(4096)
+
+            while recv_len:
+                data = client.recv(4096).decode('utf-8')
                 recv_len = len(data)
-                response+= data
-                if recv_len<4096: 
+                response += data
+
+                if recv_len < 4096:
                     break
 
-            print(f"{response}")
-            buffer = raw_input("")
-            buffer+= "\n"
-            client.send(buffer)
+            print(response, end='')
 
-    except: 
-        print("An error has occured! Exiting...")
+            buffer = input("")
+            buffer += "\n"
+            client.send(buffer.encode('utf-8'))
+
+    except Exception as e:
+        print(f"An error has occurred: {e}")
         client.close()
-
 
 def server_loop():
     global target
+    global port
 
     if not len(target):
         target = "0.0.0.0"
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((target,port))
+    server.bind((target, port))
     server.listen(5)
+
     while True:
         client_socket, addr = server.accept()
-
         client_thread = threading.Thread(target=client_handler, args=(client_socket,))
         client_thread.start()
 
 def run_command(command):
-    command = command.rstrip() #to niszczy \n 
-    try: 
-        output = subprocess.check_output(command, stderr=subprocess,STDOUT, shell=True)
-    except: 
-        output = "Failed to execute the command. \r \n"
-    return output
+    command = command.rstrip()
 
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = b"Failed to execute the command.\r\n"
+
+    return output
 
 def client_handler(client_socket):
     global upload
     global execute
     global command
+
     if len(upload_destination):
-        file_buffer = ""
-        while True: 
+        file_buffer = b""
+
+        while True:
             data = client_socket.recv(1024)
 
             if not data:
                 break
-            else: 
-                file_buffer += data 
+            else:
+                file_buffer += data
 
         try:
-            file_descriptor = open(upload_destination, "wb")
-            file_descriptor.write(file_buffer)
-            file_descriptor.close()
-            client_socket.send("Success! file saved to %s\r\n"%upload_destination)
-
-        except: 
-            client_socket.send("Failed to save file to %s\r\n"%upload_destination)
+            with open(upload_destination, "wb") as file_descriptor:
+                file_descriptor.write(file_buffer)
+            client_socket.send(f"Success! File saved to {upload_destination}\r\n".encode('utf-8'))
+        except:
+            client_socket.send(f"Failed to save file to {upload_destination}\r\n".encode('utf-8'))
 
     if len(execute):
         output = run_command(execute)
@@ -151,8 +155,14 @@ def client_handler(client_socket):
 
     if command:
         while True:
-            client.socket.send("Shell:#> ")
-            cmd_buffer += client_socket.recv(1024)
+            client_socket.send(b"Shell:#> ")
+            cmd_buffer = b""
+            while b"\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
 
-        response = run_command(cmd_buffer)
-        client_socket.send(response)
+            response = run_command(cmd_buffer.decode('utf-8'))
+            client_socket.send(response)
+
+if __name__ == "__main__":
+    main()
+
